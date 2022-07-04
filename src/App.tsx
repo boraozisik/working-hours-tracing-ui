cc21650c7c3e1ceeb4901d9f4bd6be2f236d8701
Bora Özışık <ozisikbora@gmail.com>
2022-07-04 15:46:05 +0300

    changes-updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,65 @@
 import React from "react";
-
 import "./App.css";
+import { Box, Grid, Stack, Typography, Avatar } from "@mui/material";
+import picture from "./background-image.jpg";
+import avatar from "./bgimg.jpg";
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <Box
+      sx={{
+        textAlign: "center",
+        backgroundImage: `url(${picture})`,
+        backgroundSize: "cover",
+        height: "100vh",
+      }}
+    >
+      <Grid sx={{ minHeight: "100vh", padding: 3 }}>
+        <Grid container>
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            md={8}
+            lg={8}
+            sx={{
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+          >
+            <Avatar
+              sx={{
+                height: 250,
+                width: 250,
+                marginLeft: "auto",
+                marginRight: "auto",
+              }}
+              src={avatar}
+            />
+            <Stack
+              sx={{
+                alignItems: "center",
+              }}
+            >
+              {" "}
+              <Typography
+                variant="h2"
+                sx={{ marginTop: 4, color: "#E41BBC", fontWeight: 600 }}
+              >
+                workmind
+              </Typography>
+              <Typography
+                variant="h4"
+                sx={{ marginTop: 4, color: "whitesmoke" }}
+              >
+                Working Hours Tracing System...
+              </Typography>
+            </Stack>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
 
 export default App;
